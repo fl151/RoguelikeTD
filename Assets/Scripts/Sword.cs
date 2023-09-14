457242fae4f283e890c3496d30ebfc0ffc6c8c3e
Frenk778 <121131967+Frenk778@users.noreply.github.com>
2023-09-14 13:47:13 +0300

    SwordManReady

---
+++ Assets/Scripts/Sword.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour
+{
+    [SerializeField] private float _damagee = 100f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        EnemyHealth enemyHealth = other.GetComponent<EnemyHealth>();
+
+        if (enemyHealth != null)
+        {
+            enemyHealth.TakeDamage(_damagee);
+        }
+    }
+}
