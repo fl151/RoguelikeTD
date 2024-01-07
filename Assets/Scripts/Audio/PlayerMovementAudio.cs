5b54e64e882123655f7fbbcfaa6c823e075547d8
fl151 <118074384+fl151@users.noreply.github.com>
2024-01-07 19:34:45 +0700

    добавил звуки ходьбы

---
+++ Assets/Scripts/Audio/PlayerMovementAudio.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovementAudio : MonoBehaviour
+{
+    [SerializeField] private AudioSource _audio;
+    [SerializeField] private NavMeshAgent _agent;
+
+    private void Update()
+    {
+        var velocity = _agent.velocity.magnitude;
+
+        if (velocity > 0.5f) _audio.volume = 1;
+        else _audio.volume = 0;
+    }
+
+}
