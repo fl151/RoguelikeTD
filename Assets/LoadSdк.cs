e472afc69f83de589834f36ac369d3e59c65bcb6
Frenk778 <121131967+Frenk778@users.noreply.github.com>
2024-01-11 21:09:46 +0300

    AddSdk

---
+++ Assets/LoadSdк.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using UnityEngine;
+using Agava.YandexGames;
+using UnityEngine.SceneManagement;
+//using Lean.Localization;
+
+
+public class LoadSdк : MonoBehaviour
+{
+    //[SerializeField] private LeanLocalization _leanLocalization;
+
+    //private const string EnglishLanguage = "en";
+    //private const string RussionLanguage = "ru";
+    //private const string TurkishLanguage = "tr";
+
+
+    private void Awake()
+    {
+        StartCoroutine(LoadYandexSdk());
+    }
+
+    private IEnumerator LoadYandexSdk()
+    {
+        yield return YandexGamesSdk.Initialize(() => SetLeanguage());        
+    }
+
+    private void SetLeanguage()
+    {
+        switch (YandexGamesSdk.Environment.i18n.lang)
+        {
+            //case EnglishLanguage:
+                //_leanLocalization.CurrentLanguage = Lenguage.English;
+                //break;
+
+            //case RussionLanguage:
+               // _leanLocalization.CurrentLanguage = Lenguage.Russian;
+                //break;
+
+            //case TurkishLanguage:
+               // _leanLocalization.CurrentLanguage = Lenguage.Turkish;
+                //break;
+
+            //default:
+                //_leanLocalization.CurrentLanguage = Lenguage.Russian;
+                //break;
+        }
+
+        SceneManager.LoadScene(1);
+    }
+}
