0cacfbf07dbeb8a319c02e90aca5139388105aa6
fl151 <118074384+fl151@users.noreply.github.com>
2024-01-14 23:51:20 +0700

    add pause manager and fix problem with movement audio

--- Assets/Scripts/Audio/PlayerMovementAudio.cs
+++ Assets/Scripts/Audio/PlayerMovementAudio.cs
@@ -7,14 +7,43 @@ public class PlayerMovementAudio : MonoBehaviour
     [SerializeField] private NavMeshAgent _agent;
     [SerializeField] [Range(0, 1)] private float _volume;
 
+    private bool _isGamePaused = false;
+
+    private void OnEnable()
+    {
+        PauseManager.Instance.Unpaused += OnGameUnpaused;
+        PauseManager.Instance.Paused += OnGamePaused;
+
+        _audio.volume = _volume * AudioVolumeControler.Instance.Volume;
+    }
+
+    private void OnDisable()
+    {
+        PauseManager.Instance.Unpaused -= OnGameUnpaused;
+        PauseManager.Instance.Paused -= OnGamePaused;
+    }
+
     private void Update()
     {
         var velocity = _agent.velocity.magnitude;
 
-        _audio.volume = 0;
-        if(Time.timeScale == 1)
-        {            
-            if (velocity > 0.5f) _audio.volume = _volume * AudioVolumeControler.Instance.Volume;            
-        }       
+        if(_isGamePaused == false && velocity > 0.5f)
+        {
+            _audio.enabled = true;
+        }
+        else
+        {
+            _audio.enabled = false;
+        }
+    }
+
+    private void OnGameUnpaused()
+    {
+        _isGamePaused = false;
+    }
+
+    private void OnGamePaused()
+    {
+        _isGamePaused = true;
     }
 }
