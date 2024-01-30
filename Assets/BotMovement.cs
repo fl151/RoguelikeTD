7253d71e3f64e79b83cea0de1f870a9e25f9be8b
Frenk778 <121131967+Frenk778@users.noreply.github.com>
2024-01-30 15:00:47 +0300

    Ratconfiguration

--- Assets/BotMovement.cs
+++ Assets/BotMovement.cs
@@ -19,34 +19,47 @@ public class BotMovement : MonoBehaviour
     private void GetWaypoints()
     {
         if (waypointsContainer != null)
-        {            
+        {
             waypoints = new Transform[waypointsContainer.childCount];
             for (int i = 0; i < waypointsContainer.childCount; i++)
             {
                 waypoints[i] = waypointsContainer.GetChild(i);
             }
-        }        
+        }
     }
 
     IEnumerator MoveBetweenPoints()
     {
         while (true)
+        {            
+            ShuffleWaypoints();
+
+            for (int i = 0; i < waypoints.Length; i++)
+            {
+                yield return StartCoroutine(MoveTo(waypoints[i].position));
+            }
+        }
+    }
+
+    void ShuffleWaypoints()
+    {
+        for (int i = waypoints.Length - 1; i > 0; i--)
         {
-            yield return StartCoroutine(MoveTo(waypoints[currentWaypointIndex].position));
-            
-            currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
+            int randomIndex = Random.Range(0, i + 1);
+            Transform temp = waypoints[i];
+            waypoints[i] = waypoints[randomIndex];
+            waypoints[randomIndex] = temp;
         }
     }
 
     IEnumerator MoveTo(Vector3 destination)
     {
         while (Vector3.Distance(transform.position, destination) > 0.1f)
-        {            
+        {
             transform.LookAt(destination);
-            
             transform.position = Vector3.MoveTowards(transform.position, destination, speed * Time.deltaTime);
 
             yield return null;
         }
-    }   
+    }    
 }
