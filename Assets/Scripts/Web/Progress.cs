87b4936aead06e231ca91e15c38e2cca41af8e20
fl151 <118074384+fl151@users.noreply.github.com>
2024-02-06 16:48:07 +0700

    добавил авторизацию пользователя и сохранение програсса

---
+++ Assets/Scripts/Web/Progress.cs
@@ -0,0 +1,100 @@
+using UnityEngine;
+using Agava.YandexGames;
+using UnityEngine.Events;
+
+public enum Language
+{
+    Null,
+    Ru,
+    En,
+    Tr
+}
+
+public class PlayerData
+{
+    public int CountLevelsOpened = 1;
+
+    public Language Language = Language.Null;
+}
+
+public class Progress : MonoBehaviour
+{
+    private const string _dataPrefsName = "data";
+
+    public PlayerData PlayerData;
+
+    public static Progress Instance;
+
+    public event UnityAction DataLoaded;
+    public event UnityAction<PlayerData> NeedAsk;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            transform.parent = null;
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public static void SaveDataCloud()
+    {
+        string json = JsonUtility.ToJson(Instance.PlayerData);
+
+        if (PlayerAccount.IsAuthorized)
+            PlayerAccount.SetCloudSaveData(json);
+
+        PlayerPrefs.SetString(_dataPrefsName, json);
+        PlayerPrefs.Save();
+    }
+
+    public static void SetDataFromJSON(string json)
+    {
+        var dataCloud = JsonUtility.FromJson<PlayerData>(json);
+        var dataPrefs = JsonUtility.FromJson<PlayerData>(PlayerPrefs.GetString(_dataPrefsName));
+
+        if (AreDatasEquals(dataCloud, dataPrefs))
+            SetData(dataPrefs);
+        else if (dataPrefs == default)
+            SetData(dataCloud);
+        else
+            SetData(dataCloud);
+    }
+
+    public static void SetPrefsData()
+    {
+        PlayerData dataPrefs = new PlayerData();
+
+        if (PlayerPrefs.HasKey(_dataPrefsName))
+            dataPrefs = JsonUtility.FromJson<PlayerData>(PlayerPrefs.GetString(_dataPrefsName));
+
+        SetData(dataPrefs);
+    }
+
+    public static void SetData(PlayerData data)
+    {
+        Instance.PlayerData = data;
+        Instance.DataLoaded?.Invoke();
+    }
+
+    private static void AskUserAboutProgress(PlayerData dataCloud)
+    {
+        Instance.NeedAsk?.Invoke(dataCloud);
+    }
+
+    private static bool AreDatasEquals(PlayerData data1, PlayerData data2)
+    {
+        if (data1.CountLevelsOpened != data2.CountLevelsOpened)
+            return false;
+
+        if (data1.Language != data2.Language)
+            return false;
+
+        return true;
+    }
+}
---
+++ Assets/Scripts/Web/Web.cs
@@ -0,0 +1,78 @@
+using Agava.WebUtility;
+using UnityEngine;
+using Agava.YandexGames;
+using UnityEngine.Events;
+
+public class Web : MonoBehaviour
+{
+    public event UnityAction PlayerAuth;
+
+    public static Web Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            transform.parent = null;
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnEnable()
+    {
+        WebApplication.InBackgroundChangeEvent += OnInBackgroundChange;
+    }
+
+    private void Start()
+    {
+        //if(PlayerAccount.IsAuthorized == false)
+        //    Progress.SetPrefsData();
+        //else
+        //    OnPlayerAuth();
+
+        Progress.SetPrefsData();
+
+        AuthAccount();
+    }
+
+    private void OnDisable()
+    {
+        WebApplication.InBackgroundChangeEvent -= OnInBackgroundChange;
+    }
+
+    public static void AuthAccount()
+    {
+        if (PlayerAccount.IsAuthorized == false)
+        {
+            PlayerAccount.Authorize(Instance.OnPlayerAuth);
+        }
+        else
+        {
+            Instance.OnPlayerAuth();
+        }
+    }
+
+    private void OnInBackgroundChange(bool inBackground)
+    {
+        AudioListener.pause = inBackground;
+        AudioListener.volume = inBackground ? 0f : 1f;
+
+        Time.timeScale = inBackground ? 0f : 1f;
+    }
+
+    private void OnPlayerAuth()
+    {
+        PlayerAccount.RequestPersonalProfileDataPermission();
+
+        PlayerAccount.GetCloudSaveData((data) => 
+        { 
+            Progress.SetDataFromJSON(data);
+            PlayerAuth?.Invoke();
+        });
+    }
+}
