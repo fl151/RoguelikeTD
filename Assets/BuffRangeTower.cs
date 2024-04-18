4979b435c3cd324b78203c68ad5ee39e22e63194
Frenk778 <121131967+Frenk778@users.noreply.github.com>
2024-04-18 15:03:38 +0300

    AddparticleSystemForTowers

--- Assets/BuffRangeTower.cs
+++ Assets/BuffRangeTower.cs
@@ -33,33 +33,28 @@ public class BuffRangeTower : MonoBehaviour
     //    }
     //}
 
-    public Color particleColor = Color.red; // Цвет частиц
-    public SphereCollider towerCollider; // Ссылка на Sphere Collider башни
-    public GameObject particlePrefab; // Префаб частиц
+    public Color particleColor = Color.red; 
+    public SphereCollider towerCollider;
+    public GameObject particlePrefab; 
 
-    private ParticleSystem[] particles; // Массив компонентов ParticleSystem
+    private ParticleSystem[] particles; 
 
     private void Start()
     {
         if (towerCollider != null && particlePrefab != null)
-        {
-            // Рассчитываем радиус для частиц на основе размера коллайдера башни
+        {            
             float radius = towerCollider.radius * transform.lossyScale.x;
-
-            // Создаем массив для хранения компонентов ParticleSystem
+            
             particles = new ParticleSystem[36];
-
-            // Создаем частицы вокруг башни
+            
             for (int i = 0; i < 36; i++)
             {
                 float angle = i * 10;
                 Vector3 position = transform.position + new Vector3(Mathf.Cos(angle * Mathf.Deg2Rad) * radius, 0.01f, Mathf.Sin(angle * Mathf.Deg2Rad) * radius);
-
-                // Создаем экземпляр частиц
+                                
                 GameObject particleObject = Instantiate(particlePrefab, position, Quaternion.identity);
                 ParticleSystem particleSystem = particleObject.GetComponent<ParticleSystem>();
 
-                // Настраиваем параметры частиц
                 if (particleSystem != null)
                 {
                     ParticleSystem.MainModule main = particleSystem.main;
@@ -71,8 +66,7 @@ public class BuffRangeTower : MonoBehaviour
     }
 
     private void Update()
-    {
-        // Обновляем положение частиц вокруг башни в соответствии с ее позицией
+    {        
         if (particles != null)
         {
             for (int i = 0; i < particles.Length; i++)
